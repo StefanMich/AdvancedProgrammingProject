ee8934bc553e9416d7f3eb1bf010eb5c26703b5d
Stefan Micheelsen <stufkan@gmail.com>
2015-03-30 11:07:38 +0200

    implementation of the stack as a template

---
+++ Eksamensprojekt/StackTemplate.h
@@ -0,0 +1,62 @@
+#pragma once
+template<typename T>
+class StackTemplate
+{
+public:
+	StackTemplate();
+	~StackTemplate();
+
+	void Push(T i);
+	T Pop();
+
+	bool StackEmpty();
+	T Top();
+
+private:
+	T arr[10];
+	int topIndex;
+};
+
+
+template<typename T>
+StackTemplate<T>::StackTemplate() : arr{}, topIndex{ -1}
+{
+}
+
+template<typename T>
+StackTemplate<T>::~StackTemplate()
+{
+}
+
+template<typename T>
+T StackTemplate<T>::Top()
+{
+	return arr[topIndex];
+}
+
+template<typename T>
+bool StackTemplate<T>::StackEmpty()
+{
+	if (topIndex == -1)
+		return true;
+	else return false;
+}
+
+template<typename T>
+void StackTemplate<T>::Push(T i)
+{
+	topIndex++;
+	arr[topIndex] = i;
+}
+
+template<typename T>
+T StackTemplate<T>::Pop()
+{
+	if (StackEmpty())
+		throw "underflow";
+	else
+	{
+		topIndex--;
+		return arr[topIndex + 1];
+	}
+}
