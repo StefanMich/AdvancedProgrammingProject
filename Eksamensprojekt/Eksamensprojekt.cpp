223a4bdf514f8d6062dad84f5afe80ab6d89a44d
Stefan Micheelsen <stufkan@gmail.com>
2015-03-30 11:06:38 +0200

    organization in main

--- Eksamensprojekt/Eksamensprojekt.cpp
+++ Eksamensprojekt/Eksamensprojekt.cpp
@@ -4,15 +4,24 @@
 #include "stdafx.h"
 
 #include "StackOfInt.h"
+#include "StackTemplate.h"
+#include <string>
 #include <iostream>
+
+
+using string = std::string;
+
+void StackOfIntTest();
+void StackTemplateTest();
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-
+	StackOfIntTest();
+	StackTemplateTest();
 
 	return 0;
 }
-	
+
 void StackOfIntTest()
 {
 	StackOfInt s = StackOfInt();
@@ -20,5 +29,27 @@ void StackOfIntTest()
 	s.Push(4);
 	s.Push(3);
 
-	std::cout << s.Pop() << s.Pop() << s.Pop();
+	std::cout << s.Pop() << "\n";
+	std::cout << s.Pop() << "\n";
+	std::cout << s.Pop() << "\n";
+}
+
+void StackTemplateTest(){
+	StackTemplate<int> s = StackTemplate<int>();
+	s.Push(5);
+	s.Push(4);
+	s.Push(3);
+
+	std::cout << s.Pop() << "\n";
+	std::cout << s.Pop() << "\n";
+	std::cout << s.Pop() << "\n";
+
+	StackTemplate<string> t = StackTemplate<string>();
+	t.Push("first");
+	t.Push("second");
+	t.Push("third");
+
+	std::cout << t.Pop() << "\n";
+	std::cout << t.Pop() << "\n";
+	std::cout << t.Pop() << "\n";
 }
