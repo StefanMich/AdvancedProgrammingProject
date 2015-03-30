1434b0b83ab8d736223967e648e08b03a7d00a0b
Stefan Micheelsen <stufkan@gmail.com>
2015-03-30 15:58:27 +0200

    specialisation of bool

--- Eksamensprojekt/StackTemplateBoolSpecialization.h
+++ Eksamensprojekt/StackTemplateBoolSpecialization.h
@@ -1,6 +1,13 @@
 #pragma once
 
-template<typename T, int entries>
+#include <stdexcept>
+#include <iostream>
+#include <bitset>
+
+void printArr(unsigned char* arr);
+void printChar(unsigned char ch);
+
+template<typename T>
 class StackTemplateBoolSpecialization
 {
 public:
@@ -19,44 +26,44 @@ private:
 };
 
 
-template<typename T, int entries>
-StackTemplateBoolSpecialization<T, entries>::StackTemplateBoolSpecialization() : topIndex{ -1 }
+template<typename T>
+StackTemplateBoolSpecialization<T>::StackTemplateBoolSpecialization() : topIndex{ -1 }
 {
-	arr = new T[entries];
+	arr = new T[10];
 }
 
-template<typename T, int entries>
-StackTemplateBoolSpecialization<T, entries>::~StackTemplateBoolSpecialization()
+template<typename T>
+StackTemplateBoolSpecialization<T>::~StackTemplateBoolSpecialization()
 {
 	delete arr;
 }
 
-template<typename T, int entries>
-T StackTemplateBoolSpecialization<T, entries>::Top()
+template<typename T>
+T StackTemplateBoolSpecialization<T>::Top()
 {
 	return arr[topIndex];
 }
 
-template<typename T, int entries>
-bool StackTemplateBoolSpecialization<T, entries>::StackEmpty()
+template<typename T>
+bool StackTemplateBoolSpecialization<T>::StackEmpty()
 {
 	if (topIndex == -1)
 		return true;
 	else return false;
 }
 
-template<typename T, int entries>
-void StackTemplateBoolSpecialization<T, entries>::Push(T i)
+template<typename T>
+void StackTemplateBoolSpecialization<T>::Push(T i)
 {
 	topIndex++;
 
-	if (topIndex >= entries)
+	if (topIndex >= 10)
 		throw std::out_of_range{ "overflow" };
 	else arr[topIndex] = i;
 }
 
-template<typename T, int entries>
-T StackTemplateBoolSpecialization<T, entries>::Pop()
+template<typename T>
+T StackTemplateBoolSpecialization<T>::Pop()
 {
 	if (StackEmpty())
 		throw std::out_of_range{ "underflow" };
@@ -72,8 +79,8 @@ T StackTemplateBoolSpecialization<T, entries>::Pop()
 
 // Complete bool specialization
 
-template< int entries>
-class StackTemplateBoolSpecialization<bool,entries>
+template<>
+class StackTemplateBoolSpecialization<bool>
 {
 public:
 	StackTemplateBoolSpecialization();
@@ -86,52 +93,58 @@ public:
 	bool Top();
 
 private:
-	char* arr;
+	unsigned char* arr;
 	int topIndex;
 };
 
-template< int entries>
-StackTemplateBoolSpecialization<bool,entries>::StackTemplateBoolSpecialization() : topIndex{ -1 }
+StackTemplateBoolSpecialization<bool>::StackTemplateBoolSpecialization() : topIndex{ -1 }
 {
-	int bytes = (entries / 8) + 1;
-	char arr[bytes] = {0};
-
+	arr = new unsigned char[2];
+	arr[0] = 0;
+	arr[1] = 0;
 }
 
-template<int entries>
-StackTemplateBoolSpecialization<bool,entries>::~StackTemplateBoolSpecialization()
+StackTemplateBoolSpecialization<bool>::~StackTemplateBoolSpecialization()
 {
 	delete arr;
 }
 
-template<int entries>
-bool StackTemplateBoolSpecialization<bool,entries>::Top()
+bool StackTemplateBoolSpecialization<bool>::Top()
 {
-	return arr[topIndex];
+	int positionInRepr = topIndex / 8;
+	int positionInByte = topIndex % 8;
+
+	char entry = arr[positionInRepr] << (positionInByte );
+	entry = entry >> 7;
+	bool poppedEntry = (bool)entry;
+
+	return poppedEntry;
 }
 
-template< int entries>
-bool StackTemplateBoolSpecialization<bool,entries>::StackEmpty()
+bool StackTemplateBoolSpecialization<bool>::StackEmpty()
 {
 	if (topIndex == -1)
 		return true;
 	else return false;
 }
 
-template< int entries>
-void StackTemplateBoolSpecialization<bool,entries>::Push(bool i)
+void StackTemplateBoolSpecialization<bool>::Push(bool i)
 {
-	if (topIndex >= entries)
+	if (topIndex >= 16)
 		throw std::out_of_range{ "overflow" };
 
 	
-	int positionInRepr = topIndex / 8;
+	int positionInRepr = (topIndex+1) / 8;
 
 	int index = arr[positionInRepr];
 
 	if (i == true)
 	{
-		index = index | 1 << ((8 - (topIndex % 8)) - 1);
+		int mellem = ((topIndex + 1) % 8);
+		int shift = 7 - mellem;
+
+		unsigned char bit = 1 << shift;
+		index = index | bit;
 		arr[positionInRepr] = index;
 		topIndex++;
 	}
@@ -139,12 +152,15 @@ void StackTemplateBoolSpecialization<bool,entries>::Push(bool i)
 	{
 		topIndex++;
 	}
+	
+
+	//printArr(arr);
+	//std::cout << topIndex << std::endl;
 }
 
-template< int entries>
-bool StackTemplateBoolSpecialization<bool,entries>::Pop()
+bool StackTemplateBoolSpecialization<bool>::Pop()
 {
-	std::cout << "bool specialization";
+	//std::cout << "bool specialization";
 	if (StackEmpty())
 		throw std::out_of_range{ "underflow" };
 	else
@@ -153,19 +169,32 @@ bool StackTemplateBoolSpecialization<bool,entries>::Pop()
 		int positionInByte = topIndex % 8;
 
 		// find entry in byte
-		char entry = arr[positionInRepr] << (positionInByte - 1);
-		entry = entry>> 7;
-		bool poppedEntry = (bool)entry;
+		bool poppedEntry = Top();
 
 		//reset everything to the right of topIndex to 0
-		int indexToShift = 8 - (positionInByte);
-		char index = arr[positionInRepr];
+		int indexToShift = 7 - positionInByte;
+		unsigned char index = arr[positionInRepr];
 		index = index >> indexToShift;
 		index = index << indexToShift;
+		arr[positionInRepr] = index;
 
 		topIndex--;
 
+		//printArr(arr);
 		return poppedEntry;
 	}
 }
 
+void printArr(unsigned char* arr)
+{
+	std::cout << "arr: " << std::endl;
+	printChar(arr[0]);
+	printChar(arr[1]);
+	std::cout << std::endl;
+}
+
+void printChar(unsigned char ch)
+{
+	std::bitset<8> a(ch);
+	std::cout << a << std::endl;
+}
