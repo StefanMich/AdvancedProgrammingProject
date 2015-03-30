c9bd6ab884e8823d54c7cdf242e5ee17e3e6ec43
Stefan Micheelsen <stufkan@gmail.com>
2015-03-30 09:49:06 +0200

    testmethod for the stack of int implementation

--- Eksamensprojekt/Eksamensprojekt.cpp
+++ Eksamensprojekt/Eksamensprojekt.cpp
@@ -3,9 +3,22 @@
 
 #include "stdafx.h"
 
+#include "StackOfInt.h"
+#include <iostream>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+
 	return 0;
 }
+	
+void StackOfIntTest()
+{
+	StackOfInt s = StackOfInt();
+	s.Push(5);
+	s.Push(4);
+	s.Push(3);
 
+	std::cout << s.Pop() << s.Pop() << s.Pop();
+}
