a5600e4228d7a3765dd1590e56468e67eb821a5a
Stefan Micheelsen <stufkan@gmail.com>
2015-04-01 09:22:13 +0200

    comments :O

--- Eksamensprojekt/StackTemplateValueArgument.h
+++ Eksamensprojekt/StackTemplateValueArgument.h
@@ -1,4 +1,7 @@
 #pragma once
+/// <summary>
+/// An implementation of a stack that has a template arguemnt T for the type of the elements in the stack as well as a integer template argument specifying the size of the stack
+/// </summary>
 template<typename T, int entries>
 class StackTemplateValueArgument
 {
@@ -18,24 +21,38 @@ private:
 };
 
 
+/// <summary>
+/// Initializes a new instance of the <see cref="StackTemplateValueArgument{T, entries}"/> class.
+/// </summary>
 template<typename T, int entries>
 StackTemplateValueArgument<T, entries>::StackTemplateValueArgument() : topIndex{ -1 }
 {
 	arr = new T[entries];
 }
 
+/// <summary>
+/// Finalizes an instance of the <see cref="StackTemplateValueArgument{T, entries}"/> class.
+/// </summary>
 template<typename T, int entries>
 StackTemplateValueArgument<T, entries>::~StackTemplateValueArgument()
 {
 	delete[] arr;
 }
 
+/// <summary>
+/// returns the top element of the stack without popping it
+/// </summary>
+/// <returns>The top element of the stack</returns>
 template<typename T, int entries>
 T StackTemplateValueArgument<T, entries>::Top()
 {
 	return arr[topIndex];
 }
 
+/// <summary>
+/// Returns whether the stack is empty
+/// </summary>
+/// <returns>True if the stack is empty, false otherwise</returns>
 template<typename T, int entries>
 bool StackTemplateValueArgument<T, entries>::StackEmpty()
 {
@@ -44,6 +61,10 @@ bool StackTemplateValueArgument<T, entries>::StackEmpty()
 	else return false;
 }
 
+/// <summary>
+/// Pushes the specified element to the stack.
+/// </summary>
+/// <param name="i">The element to push</param>
 template<typename T, int entries>
 void StackTemplateValueArgument<T, entries>::Push(T i)
 {
@@ -54,11 +75,15 @@ void StackTemplateValueArgument<T, entries>::Push(T i)
 	else arr[topIndex] = i;
 }
 
+/// <summary>
+/// Pops the top element from the stack
+/// </summary>
+/// <returns>The popped element</returns>
 template<typename T, int entries>
 T StackTemplateValueArgument<T, entries>::Pop()
 {
 
-	
+
 	if (StackEmpty())
 		throw std::out_of_range{ "underflow" };
 	else
